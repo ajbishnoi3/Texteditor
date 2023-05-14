cb6c790d1fba8a159dc6543d12e4ce83e6bb8da5
ajbishnoi3 <ajbishnoi3@gmail.com>
2023-05-14 07:14:09 +0530

    new things to commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import Textform from "./Components/Textform.js";
 import About from "./Components/About.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   const [mode, setMode] = useState("light");
   const toggleMode = () => {
@@ -16,11 +17,15 @@ function App() {
   };
   return (
     <>
+      <Router>
         <Navbar mode={mode} toggleMode={toggleMode} />
         <div className="container my-3">
-    
-          <Textform mode={mode} /> 
+          <Routes>
+            <Route path="/" element={<Textform mode={mode} />}></Route>
+            <Route path="/about" element={<About mode={mode} />}></Route>
+          </Routes>
         </div>
+      </Router>
     </>
   );
 }
